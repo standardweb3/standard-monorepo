8906025eb1b3530666a04dbcad2d61a32db8964a
Hyungsuk Kang <hskang9@gmail.com>
2025-01-10 09:59:01 +0900

    resolve json modules

--- packages/ui/stories/Button/Button.stories.ts
+++ packages/ui/stories/Button/Button.stories.ts
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button } from './Button';
+import { Button } from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
