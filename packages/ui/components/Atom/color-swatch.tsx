ec0c55148a4e0e74f56b5ec3a88c7afa38fe38e0
Hyungsuk Kang <hskang9@gmail.com>
2025-01-12 20:07:47 +0900

    change UI

--- packages/ui/components/Atom/color-swatch.tsx
+++ packages/ui/components/Atom/color-swatch.tsx
@@ -10,10 +10,10 @@ export function ColorSwatch({ hex, color }: ColorSwatchProps) {
     <div className="flex w-full items-center gap-4">
       <div className={`h-10 w-[100px] rounded ${color}`} />
       <div className="flex-col space-y-2">
-        <code className="w-[40px] font-bold text-xs text-gray-300">
+        <code className="w-[50px] font-bold text-xs text-gray-300">
           {color} <br/>
         </code>
-        <code className="w-16 font-mono font-regular text-xs text-gray-400">{hex}</code>
+        <code className="w-16 font-mono text-xs text-gray-400">{hex}</code>
       </div>
     </div>
   );
